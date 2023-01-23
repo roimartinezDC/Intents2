c07b59ab4f10f64cb72182cd40d3b29732c3f264
Roi Martinez <rmartinezroque@danielcastelao.org>
2023-01-23 10:22:18 +0100

    codificado envio del intent desde MainActivity

--- app/src/main/java/com/example/intents2/MainActivity.kt
+++ app/src/main/java/com/example/intents2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intents2
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnFoto = findViewById<Button>(R.id.button1)
+        btnFoto.setOnClickListener() {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePictureIntent, REQ_IMG_CAP)
+        }
+
+        val btnNum = findViewById<Button>(R.id.button2)
+        btnNum.setOnClickListener() {
+            val intent = Intent(this, SecondActivity::class.java)
+            val random = kotlin.random.Random
+            intent.putExtra("num1", random.nextInt(1,100))
+            intent.putExtra("num2", random.nextInt(1,100))
+            startActivityForResult(intent, REQ_NUM)
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -26,6 +42,12 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == REQ_IMG_CAP && data != null) {
             val imageBitMap = data.extras!!.get("data") as Bitmap
             imageView.setImageBitmap(imageBitMap)
+        }
+
+        if (resultCode != Activity.RESULT_OK || data == null) return
+
+        if (requestCode == REQ_NUM) {
+
         }
 
     }
--- app/src/main/java/com/example/intents2/SecondActivity.kt
+++ app/src/main/java/com/example/intents2/SecondActivity.kt
@@ -1,12 +1,19 @@
 package com.example.intents2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val txtVnum1 = findViewById<TextView>(R.id.textView2_1)
+
+
+
 
     }
 }
