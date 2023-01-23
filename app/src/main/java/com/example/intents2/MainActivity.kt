545437bde2c091203f65e083389e1d84364e6b8d
Roi Martinez <rmartinezroque@danielcastelao.org>
2023-01-23 09:40:25 +0100

    Botón de cámara en funcionamiento

--- app/src/main/java/com/example/intents2/MainActivity.kt
+++ app/src/main/java/com/example/intents2/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.intents2
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnFoto = findViewById<Button>(R.id.button1)
+        btnFoto.setOnClickListener() {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePictureIntent, REQ_IMG_CAP)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        if (requestCode == REQ_IMG_CAP && data != null) {
+            val imageBitMap = data.extras!!.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitMap)
+        }
 
     }
 }
